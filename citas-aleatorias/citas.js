be16f667d6ca81a4ba8442f5fd818a69feb627dd
Andy890 <vegtta89@gmail.com>
2023-07-30 18:10:58 -0300

    tusCitas

---
+++ citas-aleatorias/citas.js
@@ -0,0 +1,36 @@
+
+
+let citas = [
+    {
+      'autor': 'Albert Einstein',
+      'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
+    },
+    {
+      'autor': 'Anónimo',
+      'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
+    },
+    {
+      'autor': 'Alan Kay',
+      'texto': 'La mejor forma de predecir el futuro es inventarlo.'
+    },
+    {
+      'autor': 'Amelia Earhart',
+      'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
+    },
+    {
+      'autor': 'Aristotle',
+      'texto': 'La calidad no es un acto, es un hábito.'
+    },
+    {
+      'autor': 'Benjamin Franklin',
+      'texto': 'Dímelo y lo olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé.'
+    },
+   
+    {
+      'autor': 'John Muir',
+      'texto': 'El poder de la imaginación nos hace infinitos.'
+    }
+   
+  ];
+
+  
